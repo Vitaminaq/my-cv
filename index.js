a77ec72c8bc472714e246551400efc2ef9f965e8
fei <1109614355@qq.com>
2017-10-18 19:09:35 +0800

    'my-cv'

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 window.onload = function () {
   if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-    var content = document.getElementById('content').style.width = '100%';
-    var content = document.getElementById('content').style.fontSize = '19' + 'px';
-    // var h4 = document.getElementsByTagName('h4').style.fontSize = '24' + 'px';
-    console.log(document.getElementsByClassName("mes-title").style);
-  } else {
-  	console.log('这是在pc端');
+    var content = document.getElementById('content');
+    content.style.width = '100%';
+    content.style.fontSize = '19' + 'px';
   }
 }
