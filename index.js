54a11a57442996ae9a61d880cbcf430f6f680b64
fei <1109614355@qq.com>
2017-10-18 15:44:08 +0800

    'my-vc'

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    var content = document.getElementById('content');
+    content.style.width = '100%' + px;
+  } else {
+  	console.log('这是在pc端');
+  }
+}
