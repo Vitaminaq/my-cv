2a1cd4c04b5abfe71b0253d02da0f191778bf6f7
fei <1109614355@qq.com>
2017-10-18 16:41:17 +0800

    'my-cv'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
     var content = document.getElementById('content').style.width = '100%';
+    var content = document.getElementById('content').style.fontSize = '16' + 'px';
   } else {
   	console.log('这是在pc端');
   }
