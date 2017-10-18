b9db725f2177119893bb92c68a8e14d7f5cb6fc8
fei <1109614355@qq.com>
2017-10-18 15:53:44 +0800

    'my-cv'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-window.onload = function () {
-  if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-    var content = document.getElementById('content').style.width = '100%';
-  } else {
-  	console.log('这是在pc端');
-  }
-}
+// window.onload = function () {
+//   if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+//     var content = document.getElementById('content').style.width = '100%';
+//   } else {
+//   	console.log('这是在pc端');
+//   }
+// }
