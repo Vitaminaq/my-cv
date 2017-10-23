67f1f1563432c30b4a9567acbfce85382870c1e6
fei <1109614355@qq.com>
2017-10-23 19:31:07 +0800

    'my-cv'

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ window.onload = function () {
     content.style.width = '100%';
     content.style.fontSize = '19' + 'px';
   }
+document.body.ontouchmove = function (e) {
+    e.preventDefault();
+};
   //touchstart事件  
 var startX = 0, startY = 0;
 function touchSatrtFunc(evt) { 
     try
 	{
 	    //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等  
-
 	    var touch = evt.touches[0]; //获取第一个触点  
         var x = Number(touch.pageX); //页面触点X坐标  
         var y = Number(touch.pageY); //页面触点Y坐标  
