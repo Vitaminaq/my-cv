984c0d89b1adf982ec13d4762d1027c17fcf1039
fei <1109614355@qq.com>
2017-10-23 19:21:29 +0800

    'my-cv'

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ window.onload = function () {
     content.style.width = '100%';
     content.style.fontSize = '19' + 'px';
   }
+  //touchstart事件  
+var startX = 0, startY = 0;
+function touchSatrtFunc(evt) { 
+    try
+	{
+	    //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等  
+
+	    var touch = evt.touches[0]; //获取第一个触点  
+        var x = Number(touch.pageX); //页面触点X坐标  
+        var y = Number(touch.pageY); //页面触点Y坐标  
+        //记录触点初始位置  
+        startX = x;
+        startY = y;
+    } catch (e) {
+        alert('touchSatrtFunc：' + e.message);
+    }
+}
   document.addEventListener('touchstart', touchSatrtFunc, false);
   var _ss = document.getElementById("content");
     _ss.ontouchmove = function (ev) {
@@ -38,20 +55,4 @@ window.onload = function () {
         }
     };	 
 }
-//touchstart事件  
-function touchSatrtFunc(evt) { 
-    var startX = 0, startY = 0;
-	try
-	{
-	    //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等  
 
-	    var touch = evt.touches[0]; //获取第一个触点  
-        var x = Number(touch.pageX); //页面触点X坐标  
-        var y = Number(touch.pageY); //页面触点Y坐标  
-        //记录触点初始位置  
-        startX = x;
-        startY = y;
-    } catch (e) {
-        alert('touchSatrtFunc：' + e.message);
-    }
-}
