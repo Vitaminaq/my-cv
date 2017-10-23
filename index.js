3deddfa44e97dc5e51bd167303b5c151a8f20a27
fei <1109614355@qq.com>
2017-10-23 18:52:57 +0800

    'my-cv'

--- index.js
+++ index.js
@@ -4,4 +4,58 @@ window.onload = function () {
     content.style.width = '100%';
     content.style.fontSize = '19' + 'px';
   }
+  document.body.ontouchmove = function (e) {
+      e.preventDefault();
+  };
+  document.addEventListener('touchstart', touchSatrtFunc, false);
+  var _ss = document.getElementById("content");
+    _ss.ontouchmove = function (ev) {
+        var _point = ev.touches[0],
+        _top = _ss.scrollTop;
+        // 什么时候到底部
+        var _bottomFaVal = _ss.scrollHeight - _ss.offsetHeight;
+        // 到达顶端
+        if (_top === 0) {
+            // 阻止向下滑动
+            if (_point.clientY > startY) {
+                ev.preventDefault();
+            } else {
+                // 阻止冒泡
+                // 正常执行
+                ev.stopPropagation();
+            }
+        } else if (_top === _bottomFaVal) {
+            // 到达底部
+            // 阻止向上滑动
+            if (_point.clientY < startY) {
+                ev.preventDefault();
+            } else {
+                // 阻止冒泡
+                // 正常执行
+                ev.stopPropagation();
+            }
+        } else if (_top > 0 && _top < _bottomFaVal) {
+            ev.stopPropagation();
+        } else {
+            ev.preventDefault();
+        }
+    };	 
+}
+//touchstart事件  
+function touchSatrtFunc(evt) { 
+    var startX = 0, startY = 0;
+	try
+	{
+	    //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等  
+
+	    var touch = evt.touches[0]; //获取第一个触点  
+        var x = Number(touch.pageX); //页面触点X坐标  
+        var y = Number(touch.pageY); //页面触点Y坐标  
+        //记录触点初始位置  
+        startX = x;
+        startY = y;
+        console.log(startY,startX);
+    } catch (e) {
+        alert('touchSatrtFunc：' + e.message);
+    }
 }
